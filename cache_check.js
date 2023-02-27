aee6e50b9f699c347123a954ec2ddff08aa08b66
KirilDonec <126416531+KirilDonec@users.noreply.github.com>
2023-02-27 09:36:37 +0200

    Update Cache

--- cache_check.js
+++ cache_check.js
@@ -1,14 +1,50 @@
-// Получаем доступ к объекту Cache API в браузере
-caches.open('my-cache').then(function(cache) {
-  // Сравниваем кэш на сайте и кэш на устройстве пользователя
-  cache.match('/path/to/my/file').then(function(response) {
-    // Загружаем файл с сервера
-    fetch('/path/to/my/file').then(function(newResponse) {
-      // Если кэш на сайте отличается от кэша на устройстве пользователя
-      if (response.status !== newResponse.status) {
-        // Сохраняем новый кэш на устройстве пользователя
-        cache.put('/path/to/my/file', newResponse.clone());
-      }
-    });
-  });
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to make a fetch
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to store it in the cache
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+    );
 });
